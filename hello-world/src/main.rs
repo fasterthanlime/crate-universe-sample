8032a9f55752a92a07fa1be54ee2e0110c5dc2a3
Amos Wenger <amoswenger@gmail.com>
2021-12-14 19:24:44 +0100

    Actually use rcgen

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
+    let key = rcgen::generate_simple_self_signed(vec!["example.org".into()]).unwrap();
+    println!("got it: {:x?}", key.get_key_identifier());
 }
